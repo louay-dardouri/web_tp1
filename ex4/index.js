ec4f5fbc352d0214bf43f07ff0e72502d38fc905
louay-dardouri <louay.dardouri@insat.ucar.tn>
2025-02-14 10:31:38 +0100

    ex4

---
+++ ex4/index.js
@@ -0,0 +1,12 @@
+function getRandomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+document.querySelector("body").addEventListener("click", (e) => {
+  if (e.target.tagName.toLowerCase() == "li") {
+    e.target.style.backgroundColor = getRandomColor();
+  }
+});
